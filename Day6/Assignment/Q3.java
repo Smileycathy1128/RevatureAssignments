da01d64bcf92b88bdd0343e8c31f65bfb73b5a77
Cathy Nguyen <Smileycathy1128@users.noreply.github.com>
2021-10-26 18:07:50 -0400

    Add files via upload

---
+++ Day6/Assignment/Q3.java
@@ -0,0 +1,26 @@
+package Day6.Assignment;
+
+import java.util.Scanner;
+
+class average {
+    static void printAvg(float n1, float n2, float n3) {
+        System.out.println("Average: "+ (n1+n2+n3)/3);
+    }
+}
+
+public class Q3 {
+    public static void main(String[] args) {
+        float n1, n2, n3;
+        System.out.println("Enter three numbers: ");
+        Scanner scan = new Scanner(System.in);
+        n1 = scan.nextFloat();
+        n2 = scan.nextFloat();
+        n3 = scan.nextFloat();
+        scan.close();
+        average.printAvg(n1, n2, n3);
+    }
+}
+
+// Q3: Print the average of three numbers entered by user
+//     by creating a class named 'Average' 
+//     and having a method to calculate and print the average.
---
+++ Day6/Assignment/Q4.java
@@ -0,0 +1,19 @@
+package Day6.Assignment;
+
+public class Q4 {
+    
+}
+// Q4: Calculate the percentage of marks obtained 
+//     in three subjects by student A and in four subjects by student B.
+//     Create an abstract class 'Marks' with an abstract method 'getPercentage'. 
+//     It is inherited by two other classes 'A' and 'B' each having 
+//     a method with the same name which returns the percentage of the students. 
+//     The constructor of student A takes the marks in three subjects as its parameters 
+//     and the marks in four subjects as its parameters for student B.
+//     Create an object for eac of the two classes 
+//     and print the percentage of marks for both the students.
+// Q5: Create an abstract class 'Bank' with an abstract method 'getBalance'. 
+//     $100, $150 and $200 are deposited in banks A, B and C respectively. 
+//     'BankA', 'BankB' and 'BankC' are subclasses of class 'Bank', 
+//     each having a method named 'getBalance'. 
+//     Call this method by creating an object of each of the three classes.
